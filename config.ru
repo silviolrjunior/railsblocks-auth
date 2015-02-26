c56abefd7a65040aa45d5703e5ec4367fe54de71
Celso Fernandes <fernandes@zertico.com>
2015-02-26 12:48:16 -0300

    Initial Devise Support

---
+++ config.ru
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler'
+
+require 'combustion'
+
+Combustion.initialize! :all
+run Combustion::Application
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,17 +1,16 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
-require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'shoulda-matchers'
 require 'factory_girl'
 require 'database_cleaner'
 
-Dir[ComponentTemplate::Engine.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Railsblocks::Auth::Engine.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 # load factories
-Dir[ComponentTemplate::Engine.root.join("spec/factories/**/*.rb")].each {|f| require f}
+Dir[Railsblocks::Auth::Engine.root.join("spec/factories/**/*.rb")].each {|f| require f}
 
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
